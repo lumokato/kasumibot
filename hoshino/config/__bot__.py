77cfdeb7228bd85d227f9b8d24fee1b48111e6c0
lumokato <suzto@qq.com>
2021-05-21 00:01:11 +0800

    .

--- hoshino/config/__bot__.py
+++ hoshino/config/__bot__.py
@@ -31,7 +31,7 @@ RES_URL = 'http://127.0.0.1:5000/static/'
 # 如欲启用新模块，请认真阅读部署说明，逐个启用逐个配置
 # 切忌一次性开启多个
 MODULES_ON = {
-    #'botmanage',
+    'botmanage',
     #'dice',
     'groupmaster',
     # 'hourcall',
